a5b804911ea5dc27ad1e1d9dc5820df1671fb49a
zCaps <gabriel.abrantes-alfredo@etu.univ-smb.fr>
2025-02-07 16:15:13 +0100

    Initialisation projet

---
+++ src/main/java/com/traderalerting/MainApplication.java
@@ -0,0 +1,24 @@
+package com.traderalerting;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
+
+@RestController
+@RequestMapping("/api")
+class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Trader!";
+    }
+}
